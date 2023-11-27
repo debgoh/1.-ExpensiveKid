d41d97d0cdec5a14df367241f7234127ad8092ed
debgoh <goh.hh.deborah@gmail.com>
2023-11-27 13:46:47 +0800

    change port 3000 to 80

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.set('view engine', 'ejs');
 
